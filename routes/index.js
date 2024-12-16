5ad5a02a396a27518f8615176ccdb15a3bcee240
OBEONE67 <filmloloto67@gmail.com>
2024-12-16 17:31:30 +0700

    Changes to be committed:

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Yamaha Shop' });
 });
 
 module.exports = router;
